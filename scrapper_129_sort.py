103b4f12f6752fffc4c9000a9c20421ee00ef487
studentfordemo <64936561+studentfordemo@users.noreply.github.com>
2021-06-21 15:22:07 +0530

    Add files via upload

---
+++ scrapper_129_sort.py
@@ -0,0 +1,24 @@
+import csv
+
+data = []
+
+with open("websitedata.csv", "r") as f:
+    csvreader = csv.reader(f)
+    for row in csvreader: 
+        data.append(row)
+
+headers = data[0]
+planet_data = data[1:]
+
+#Converting all planet names to lower case
+for data_point in planet_data:
+    data_point[0] = data_point[0].lower()
+
+#Sorting planet names in alphabetical order
+planet_data.sort(key=lambda planet_data: planet_data[0])
+
+
+with open("dataset_2_sorted.csv", "a+") as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(planet_data)
